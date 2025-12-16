44ebf688614a0d56a13b26d4cb4d1aabbd4e4319
omaralaa252006-ship-it <omaralaa252006@gmail.com>
2025-12-17 01:26:56 +0200

    LED changes

--- LED.h
+++ LED.h
@@ -1,23 +1,23 @@
 #ifndef _LED_H
 #define _LED_H
 
-#include "Components/Gate.h"
+#include "Components/Component.h"
+#include "Components/InputPin.h"
 
-class LED :public Gate
+class LED : public Component
 {
+	InputPin m_InputPin;
 public:
-	LED(const GraphicsInfo& r_GfxInfo, int r_FanOut);
-	virtual void Operate();	//Calculates the output of the OR gate
-	virtual void Draw(Output* pOut);	//Draws 2-input gate
+	LED(const GraphicsInfo& r_GfxInfo);
+	virtual void Operate();
+	virtual void Draw(Output* pOut);
 
-	virtual Component* clone() override;
-
-	virtual int GetOutPinStatus();	//returns status of outputpin if LED, return -1
-	virtual int GetInputPinStatus(int n);	//returns status of Inputpin # n if SWITCH, return -1
+	virtual int GetOutPinStatus();	//returns -1
+	virtual int GetInputPinStatus(int n);	//returns status of Inputpin # n
 
 	virtual void setInputPinStatus(int n, STATUS s);	//set status of Inputpin # n, to be used by connection class.
 
-
+	virtual Component* clone() override;
 };
 
 #endif
